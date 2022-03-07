19362e78356b1b06888e05b7ca1dbca9ed78d2d3
Aditya Prakash <94276846+adityaprkshiitp@users.noreply.github.com>
2022-03-07 22:52:25 +0530

    changes made to learn pipeline

--- textutils/textutils/urls.py
+++ textutils/textutils/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', views.index, name='homepage'),
+    path('division', views.division, name='divide'),
+    path('subtraction', views.subtraction, name='subtract'),
+    path('addition', views.addition, name='add'),
+    path('multiplication', views.multiplication, name='multiply'),
 ]
--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello Aditya")
+    return HttpResponse('''<h1><center>HOME</center></h1><br><a href="http://127.0.0.1:8000/division"> <b>Divide here</b></a>
+    <br><a href="http://127.0.0.1:8000/subtraction"> <b>Subtract here</b></a>
+    <br><a href="http://127.0.0.1:8000/addition"> <b>Add here</b></a>
+    <br><a href="http://127.0.0.1:8000/multiplication"> <b>Multiply here</b></a>''')
+
+def division(request):
+    return HttpResponse('''<h1>Division</h1> <a href="http://127.0.0.1:8000"> Go back to Homepage</a>''')
+
+def subtraction(request):
+    return HttpResponse('''<h1>Subtraction</h1> <a href="http://127.0.0.1:8000"> Go back to Homepage</a>''')
+
+def addition(request):
+    return HttpResponse('''<h1>Addition</h1> <a href="http://127.0.0.1:8000"> Go back to Homepage</a>''')
+
+def multiplication(request):
+    return HttpResponse('''<h1>Multiplies</h1> <a href="http://127.0.0.1:8000"> Go back to Homepage</a>''')
