114d8cec4578daf65410e02728c141b76f7570f5
Aditya Prakash <94276846+adityaprkshiitp@users.noreply.github.com>
2022-03-08 23:58:34 +0530

    templates

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -1,10 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('''<h1><center>HOME</center></h1><br><a href="http://127.0.0.1:8000/division"> <b>Divide here</b></a>
-    <br><a href="http://127.0.0.1:8000/subtraction"> <b>Subtract here</b></a>
-    <br><a href="http://127.0.0.1:8000/addition"> <b>Add here</b></a>
-    <br><a href="http://127.0.0.1:8000/multiplication"> <b>Multiply here</b></a>''')
+    return render(request,'index.html')
 
 def division(request):
     return HttpResponse('''<h1>Division</h1> <a href="http://127.0.0.1:8000"> Go back to Homepage</a>''')
